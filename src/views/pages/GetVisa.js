de287c2231a1a83d959b5da4a6f10dbe2c276965
dsarkar86 <69214649+dsarkar86@users.noreply.github.com>
2021-09-07 18:49:56 +0530

    added links to get visa page

--- src/views/pages/GetVisa.js
+++ src/views/pages/GetVisa.js
@@ -38,7 +38,7 @@ function GetVisapage() {
                 
                 <div className="flag_holder">
                 <Row>
-                    <Col md="3" className="flags mb-3">
+                    /*<Col md="3" className="flags mb-3">
                     <img
                     alt="..."
                     className="pull-left mr-2"
@@ -133,8 +133,55 @@ function GetVisapage() {
                     src={require("../../assets/img/flagsb/eg.png").default}
                     ></img>
                     <p >Egypt</p>
-                    </Col>
-              
+                    </Col>*/
+					<Col to="/getvisadetails/armenia" tag={Link} md="3" className="flags mb-3" >
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/am.png").default} ></img>
+							<p >Armenia</p>
+						</Col>
+						<Col to="/getvisadetails/australia" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/au.png").default} ></img>
+							<p >Australia</p>
+						</Col>
+						<Col to="/getvisadetails/azerbaijan" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/az.png").default} ></img>
+							<p >Azerbaijan</p>
+						</Col>
+						<Col to="/getvisadetails/bahamas" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/bs.png").default} ></img>
+							<p >Bahamas</p>
+						</Col>
+						<Col to="/getvisadetails/bahrain" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/bh.png").default} ></img>
+							<p >Bahrain</p>
+						</Col>
+						<Col to="/getvisadetails/bangladesh" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/bd.png").default} ></img>
+							<p >Bangladesh</p>
+						</Col>
+						<Col to="/getvisadetails/benin" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/bj.png").default} ></img>
+							<p >Benin</p>
+						</Col>
+						<Col to="/getvisadetails/brazil" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/br.png").default} ></img>
+							<p >Brazil</p>
+						</Col>
+						<Col to="/getvisadetails/cambodia" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/kh.png").default} ></img>
+							<p >Cambodia</p>
+						</Col>
+						<Col to="/getvisadetails/canada" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/ca.png").default} ></img>
+							<p >Canada</p>
+						</Col>
+						<Col to="/getvisadetails/colombia" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/co.png").default} ></img>
+							<p >Colombia</p>
+						</Col>
+						<Col to="/getvisadetails/egypt" tag={Link} md="3" className="flags mb-3">
+							<img alt="..." className="pull-left mr-2" src={require("../../assets/img/flagsb/eg.png").default} ></img>
+							<p >Egypt</p>
+						</Col>
                 </Row>
               
                 </div>
